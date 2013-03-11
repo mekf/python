ba627e3e6def6a9a2b72e042044cf62d7cbd64a9
Minh Ha <nhat.minh.ha@gmail.com>
2013-03-12 11:54:28 +1300

    finished sorting, not commenting yet

---
+++ sorting.py
@@ -0,0 +1,17 @@
+def InsertsionSort(A):
+	for j in range (1, len(A)):
+		key = A[j]
+		i = j - 1
+		while (i >= 0) and (A[i] > key):
+			A[i+1] = A[i]
+			i -= 1
+		A[i+1] = key
+
+x = input("define an array \n> ")
+
+print "input:"
+print x
+
+InsertsionSort(x)
+print "output:"
+print x
