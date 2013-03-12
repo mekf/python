9fa433cab6e6780325ce401ac9b50025e94ef185
Minh Ha <nhat.minh.ha@gmail.com>
2013-03-13 01:16:42 +1300

    extra in ex11

--- python-the-hard-way/ex11_Asking_Questions.py
+++ python-the-hard-way/ex11_Asking_Questions.py
@@ -8,4 +8,8 @@ height = raw_input()
 print "How heavy are you?",
 weight = raw_input()
 
+#this will also work:
+sleep_time = raw_input("How many hours do you sleep per night? ")
+
 print "Age: %s years old, Height: %s cm, Weight: %s kg" % (age, height, weight)
+print "Sleep time: %s hours per night." % sleep_time
