e5a9fe3922e81dea74e08cf169d35c949f292ff4
Minh Ha <nhat.minh.ha@gmail.com>
2013-03-13 01:13:54 +1300

    finished ex11

---
+++ python-the-hard-way/ex11_Asking_Questions.py
@@ -0,0 +1,11 @@
+print "Exercise 11: Asking Questions \n"
+
+# with the comma, age is one the same line as the question
+print "How old are you?",
+age = raw_input()
+print "How tall are you?",
+height = raw_input()
+print "How heavy are you?",
+weight = raw_input()
+
+print "Age: %s years old, Height: %s cm, Weight: %s kg" % (age, height, weight)
