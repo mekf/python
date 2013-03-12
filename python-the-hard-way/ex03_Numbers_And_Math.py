30ac918f0cfd2fc53863d0db677dbf1f6969795c
Minh Ha <nhat.minh.ha@gmail.com>
2013-03-13 00:45:08 +1300

    finished ex03

---
+++ python-the-hard-way/ex03_Numbers_And_Math.py
@@ -0,0 +1,31 @@
+print "Exercise 3: Numbers And Math \n"
+
+print "Chickens count:"
+
+print "Hens", 25 + 30 / 6
+print "Roosters", 100 - 25 * 3 % 4
+# 100 - 75 modulo 4 (75/4 = 18 + 3)
+# should be 97
+
+print "Eggs count:"
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+# 5 % 2 - 0 + 6
+# should be 7
+print "With float:"
+print 3 + 2 + 1 - 5 + 4 % 2 - 1. / 4 + 6
+# should be 6.75
+
+print "Is it true that 3 + 2 < 5 - 7?"
+
+print 3 + 2 < 5 - 7
+
+print "What is 3 + 2?", 3 + 2
+print "What is 5 - 7?", 5 - 7
+
+print "Oh, that's why it's False."
+
+print "How about some more."
+
+print "Is it greater?", 5 > -2
+print "Is it greater or equal?", 5 >= -2
+print "Is it less or equal?", 5 <= -2
